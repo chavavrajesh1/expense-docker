bafcc1f9ca7e427b26be5fa045f66774ba7ab9ee
chava venkat <chavavrajesh1.com>
2024-06-26 19:19:53 +0530

    functions

--- install-docker.sh
+++ install-docker.sh
@@ -9,5 +9,5 @@ yum-config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.r
 yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 systemctl start docker
 systemctl enable docker
-usermod -aG ec2-user
+usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
